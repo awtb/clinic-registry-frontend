2a8ee42bff9abbb95a0ad4ffde5d2594099ff39d
Ilyas Qalandarzoda <ilyas.qalandarzoda@icloud.com>
2026-01-04 12:56:29 +0500

    refactor(api-client): Reorganize `auth` endpoint.

--- src/lib/server/api/endpoints/auth.ts
+++ src/lib/server/api/endpoints/auth.ts
@@ -5,20 +5,26 @@ import type { ApiResult } from "$lib/server/api/types"
 
 export type Token = z.infer<typeof TokenSchema>
 
+type Client = ReturnType<typeof buildClient>
+
+export const buildTokenMethod = (client: Client) => {
+    return async (email: string, password: string): Promise<ApiResult<Token>> => {
+        const body = new URLSearchParams({ username: email, password })
+
+        return await client.request({
+            path: "/auth/token",
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body,
+            schema: TokenSchema,
+        })
+    }
+}
+
 export const buildAuthApi = (fetchFn: typeof fetch) => {
-  const client = buildClient(fetchFn)
+    const client = buildClient(fetchFn)
 
-  const token = async (email: string, password: string): Promise<ApiResult<Token>> => {
-    const body = new URLSearchParams({ username: email, password })
-
-    return await client.request({
-      path: "/auth/token",
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body,
-      schema: TokenSchema,
-    })
-  }
-
-  return { token }
+    return {
+        token: buildTokenMethod(client),
+    }
 }
