4dbf3600a0698015e5668f85ebbbea20c5d5c8e9
Ilyas Qalandarzoda <ilyas.qalandarzoda@icloud.com>
2026-02-05 14:33:59 +0500

    feat(users): Add email field to user schema and handle empty patient data in UI

--- src/lib/schemas/user.ts
+++ src/lib/schemas/user.ts
@@ -4,6 +4,7 @@ export const UserSchema = z.object({
     id: z.string(),
     first_name: z.string(),
     last_name: z.string(),
+    username: z.string().optional(),
     email: z.string(),
     role: z.string(),
 })
@@ -11,7 +12,8 @@ export const UserSchema = z.object({
 export const UserCreateSchema = z.object({
     first_name: z.string().min(1, "Имя обязательно"),
     last_name: z.string().min(1, "Фамилия обязательна"),
-    email: z.string().email("Введите корректный email"),
+    username: z.string().min(1, "Имя пользователя обязательно"),
+    email: z.email("Введите корректный email"),
     role: z.string().min(1, "Роль обязательна"),
     password: z.string().min(6, "Пароль должен быть не короче 6 символов"),
 })
