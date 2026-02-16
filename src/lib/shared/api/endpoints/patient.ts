b590e90462d9276370a33b094855564a3582ca17
Ilyas Qalandarzoda <ilyas.qalandarzoda@icloud.com>
2026-02-16 11:04:48 +0500

    fix(api-client): Fix patient-create schema.

--- src/lib/shared/api/endpoints/patient.ts
+++ src/lib/shared/api/endpoints/patient.ts
@@ -1,40 +1,40 @@
 import { createPageSchema } from "$lib/schemas/base"
 import { PatientSchema, PatientCreateSchema } from "$lib/schemas/patient"
+import type { z } from "zod"
 import type { buildHttpClient } from "../client"
 
 type Client = ReturnType<typeof buildHttpClient>
 const PatientPageSchema = createPageSchema(PatientSchema)
 
 const buildGetAllPatientsMethod = (httpClient: Client) => {
-    return async (page: number, page_size: number) => {
-        return await httpClient.request({
-            path: "patients/",
-            method: "GET",
-            params: {
-                page,
-                page_size
-            },
-            schema: PatientPageSchema
-        })
-    }
+  return async (page: number, page_size: number) => {
+    return await httpClient.request({
+      path: "patients/",
+      method: "GET",
+      params: {
+        page,
+        page_size,
+      },
+      schema: PatientPageSchema,
+    })
+  }
 }
 
-
 const buildCreatePatientMethod = (httpClient: Client) => {
-    return async (payload: typeof PatientCreateSchema) => {
-        return await httpClient.request({
-            path: "patients/",
-            method: "POST",
-            body: JSON.stringify(payload),
-            headers: { "Content-Type": "application/json" },
-            schema: PatientSchema
-        })
-    }
+  return async (payload: z.infer<typeof PatientCreateSchema>) => {
+    return await httpClient.request({
+      path: "patients/",
+      method: "POST",
+      body: JSON.stringify(payload),
+      headers: { "Content-Type": "application/json" },
+      schema: PatientSchema,
+    })
+  }
 }
 
 export const buildPatientsClient = (httpClient: ReturnType<typeof buildHttpClient>) => {
-    return {
-        getAll: buildGetAllPatientsMethod(httpClient),
-        create: buildCreatePatientMethod(httpClient)
-    }
+  return {
+    getAll: buildGetAllPatientsMethod(httpClient),
+    create: buildCreatePatientMethod(httpClient),
+  }
 }
