983ebb912c3cac63913db865c7a5365a3825dec5
Ilyas Qalandarzoda <ilyas.qalandarzoda@icloud.com>
2026-02-16 11:07:37 +0500

    refactor(patients): Delete server-side code.

--- src/routes/patients/+page.server.ts
+++
@@ -1,8 +0,0 @@
-export const actions = {
-  new: async ({ request }) => {
-    const form = await request.formData()
-    const payload = Object.fromEntries(form)
-
-    return { ok: true, payload }
-  },
-}
